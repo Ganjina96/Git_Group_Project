d4eb63238a9bf1b4a160b2f781ad3a6f17f2b860
Viktoriia Mishchenko <viktoriiamish@gmail.com>
2022-04-07 10:22:47 -0400

    Victoria first commit

--- src/Setup.java
+++ src/Setup.java
@@ -2,4 +2,6 @@ public class Setup {
     // setting up project for everyone
     //sasha started making some changes
     //sasha continue making some changes
+    // Victoria started making some changes
+
 }
