faa58ddbf3931a21dea9d1c954c459b75b898e1d
Viktoriia M <88465922+viktoriiamishchenko@users.noreply.github.com>
2022-04-09 19:58:08 -0400

    Victoria (#15)
    * Victoria created cat class
    
    * Victoria created cat class
    
    * Victoria created conflict
    
    Co-authored-by: pakalexey1 <98862552+pakalexey1@users.noreply.github.com>

--- src/Setup.java
+++ src/Setup.java
@@ -12,7 +12,11 @@ public class Setup {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
+        System.out.println("cat");
+
+
         System.out.println("Bye World");
+
 
 
     }
