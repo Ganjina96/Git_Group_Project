a78689213d22d5e7a5e874a115db20b256cba1f0
Veronica Pak <nikampak@gmail.com>
2022-05-02 11:03:03 -0400

    Created Poodle class

---
+++ src/dog_package/Poodle.java
@@ -0,0 +1,8 @@
+package dog_package;
+
+public class Poodle {
+    public static void main(String[] args) {
+        DogOne poodleOne = new DogOne("Jamie", "Poodle",5, "Brown" );
+        System.out.println(poodleOne);
+    }
+}
